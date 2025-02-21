99691c5f7418b67ee9f7f8767cfe82c65e4442bd
Decator <decator.c@proton.me>
2025-02-21 15:04:44 -0500

    Make it deployable

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,39 @@
-use log::error;
+use log::{info, warn, error};
 use rinja::Template as _;
 use serde::{Deserialize, Serialize};
 use std::fs::{self, File};
 use std::io::{Read, Write};
+use std::str::FromStr;
 use tiny_http::{Header, Method, Request, Response};
+use std::time::Duration;
+use signal_hook::consts::SIGHUP;
+use std::sync::{Arc, atomic::{Ordering, AtomicBool}};
+use std::path::Path;
+
 #[allow(dead_code)]
 mod uri;
 
-#[derive(Debug, Clone, Default, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 struct Config {
+    #[serde(default = "Config::default_bind")]
+    bind: String,
     #[serde(default)]
     apps: Vec<Entry>,
+}
+
+impl Config {
+    fn default_bind() -> String {
+        String::from("127.0.0.1:8333")
+    }
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            bind: Self::default_bind(),
+            apps: Vec::default(),
+        }
+    }
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -28,18 +51,7 @@ enum Icon {
     Local { path: String },
 }
 
-fn main() -> eyre::Result<()> {
-    use log::LevelFilter;
-    env_logger::Builder::new()
-        .filter(None, LevelFilter::Debug)
-        .init();
-
-    let config_dir = dirs::config_dir().expect("config directory").join("dash");
-    let config_file = config_dir.join("config.toml");
-    if !config_dir.exists() {
-        fs::create_dir_all(&config_dir)?;
-    }
-
+fn load_config(config_file: &Path) -> eyre::Result<Config> {
     let config = if !config_file.exists() {
         let c = Config::default();
         let t = toml::ser::to_string_pretty(&c)?;
@@ -50,6 +62,32 @@ fn main() -> eyre::Result<()> {
         let contents = fs::read_to_string(&config_file)?;
         toml::de::from_str(&contents)?
     };
+    Ok(config)
+}
+
+fn main() -> eyre::Result<()> {
+    use log::LevelFilter;
+    env_logger::Builder::new()
+        .filter(None, LevelFilter::Debug)
+        .init();
+
+    let reload_state = Arc::new(AtomicBool::new(false));
+    signal_hook::flag::register(SIGHUP, reload_state.clone())?;
+
+    let config_dir = dirs::config_dir().expect("config directory").join("dash");
+    let config_file = config_dir.join("config.toml");
+    if !config_dir.exists() {
+        fs::create_dir_all(&config_dir)?;
+    }
+
+    let mut config = load_config(&config_file)?;
+    let bind = match std::net::SocketAddr::from_str(&config.bind) {
+        Ok(b) => b,
+        Err(e) => {
+            error!("Invalid bind address: {e}");
+            std::process::exit(1);
+        }
+    };
 
     let mut local_icons = Vec::new();
     for app in config.apps.iter() {
@@ -58,23 +96,53 @@ fn main() -> eyre::Result<()> {
         }
     }
 
-    let server = tiny_http::Server::http("127.0.0.1:3000").unwrap();
+    let server = tiny_http::Server::http(bind)
+        .map_err(|e| eyre::eyre!("Failed to run server: {e}"))?;
+
     loop {
         // blocks until the next request is received
-        let request = match server.recv() {
+        let Some(request) = (match server.recv_timeout(Duration::from_millis(420)) {
             Ok(rq) => rq,
             Err(e) => {
                 error!("{}", e);
                 break;
             }
+        }) else {
+            if reload_state.swap(false, Ordering::Relaxed) {
+                info!("Reloading state...");
+                match load_config(&config_file) {
+                    Ok(c) => {
+                        config = c;
+                        info!("Reloaded config from {config_file:?}");
+                    },
+                    Err(e) => {
+                        error!("Failed to reload config file {config_file:?}: {e}");
+                        warn!("Using old configuration");
+                    }
+                }
+                
+            }
+            continue;
         };
 
         let method = request.method();
-        let host = request
+        let Some(host) = request
             .headers()
             .iter()
             .find(|x| x.field.as_str().as_str().eq_ignore_ascii_case("host"))
-            .expect("Expected \"host\" request header!");
+            .map(|x| x.value.as_str())
+        else {
+            error!("Request is missing \"Host\" header!");
+            respond_or_log(
+                request,
+                Response::from_string("Missing \"Host\" header").with_status_code(400),
+            );
+            continue;
+        };
+        let (host, _port) = match host.rsplit_once(':') {
+            Some((host, port)) => (host, Some(port)),
+            None => (host, None),
+        };
 
         let Ok(url) = uri::Uri::new(request.url()) else {
             respond_or_log(request, Response::empty(400));
@@ -87,7 +155,7 @@ fn main() -> eyre::Result<()> {
                     .apps
                     .iter()
                     .map(|app| match uri::Uri::new(&app.url) {
-                        Ok(url) if url.host.is_none() => {
+                        Ok(url) if url.host.is_none_or(str::is_empty) => {
                             let mut url = uri::UriOwned::from(url);
                             url.host = Some(host.to_string());
                             let mut e = app.clone();
@@ -97,7 +165,15 @@ fn main() -> eyre::Result<()> {
                         _ => app.clone(),
                     })
                     .collect::<Vec<_>>();
-                let html = Template { apps: &apps }.render()?;
+                let Ok(html) = Template { apps: &apps }.render() else {
+                    error!("Failed to render template!");
+                    respond_or_log(
+                        request,
+                        Response::from_string("Failed to render response")
+                            .with_status_code(500),
+                    );
+                    continue;
+                };
                 respond_or_log(
                     request,
                     Response::from_string(html).with_header(
@@ -109,7 +185,7 @@ fn main() -> eyre::Result<()> {
                 && local_icons.iter().any(|x| x == &path["/icon/".len()..]) =>
             {
                 let path = &path["/icon/".len()..];
-                let Ok(mut f) = File::open(path) else {
+                let Ok(mut f) = File::open(&path) else {
                     // TODO: don't assume not-found
                     respond_or_log(request, Response::empty(404));
                     continue;
